19bda4af8f3ece8a876dcf4af51d91bb511c77f0
Bista, Dipendra <Dipendra.Bista@cotiviti.com>
2023-03-22 22:31:33 +0545

    Dummy data preparation for medication and Drone

---
+++ src/main/resources/data.sql
@@ -0,0 +1,17 @@
+INSERT INTO DRONE (serial_number, battery_capacity, model, state, weight_limit) VALUES('DRONE001', 100, 'LIGHTWEIGHT', 'IDLE', 120);
+INSERT INTO DRONE (serial_number, battery_capacity, model, state, weight_limit) VALUES('DRONE002', 99, 'MIDDLEWEIGHT', 'IDLE', 220);
+INSERT INTO DRONE (serial_number, battery_capacity, model, state, weight_limit) VALUES('DRONE003', 88, 'CRUISERWEIGHT', 'IDLE',300);
+INSERT INTO DRONE (serial_number, battery_capacity, model, state, weight_limit) VALUES('DRONE004', 95, 'HEAVYWEIGHT', 'IDLE', 450);
+INSERT INTO DRONE (serial_number, battery_capacity, model, state, weight_limit) VALUES('DRONE005', 90, 'HEAVYWEIGHT', 'IDLE', 500);
+
+
+INSERT INTO MEDICATION (code, name, weight) VALUES('MED001', 'Melanotan', 10);
+INSERT INTO MEDICATION (code, name, weight) VALUES('MED002', 'Clemastine', 20);
+INSERT INTO MEDICATION (code, name, weight) VALUES('MED003', 'Ibuprofen', 10);
+INSERT INTO MEDICATION (code, name, weight) VALUES('MED004', 'Acetaminophen', 10);
+INSERT INTO MEDICATION (code, name, weight) VALUES('MED005', 'Adderall', 15);
+INSERT INTO MEDICATION (code, name, weight) VALUES('MED006', 'Amitriptyline', 25);
+INSERT INTO MEDICATION (code, name, weight) VALUES('MED007', 'Dapagliflozin', 10);
+INSERT INTO MEDICATION (code, name, weight) VALUES('MED008', 'Detrol', 5);
+INSERT INTO MEDICATION (code, name, weight) VALUES('MED009', 'Felodipine', 10);
+INSERT INTO MEDICATION (code, name, weight) VALUES('MED0010', 'Zaleplon', 50);
