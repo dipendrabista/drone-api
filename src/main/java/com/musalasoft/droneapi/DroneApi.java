c65399f1a8759fdc6044a581685d497ca7296bfb
Bista, Dipendra <Dipendra.Bista@cotiviti.com>
2023-03-22 21:49:16 +0545

    Project skeleton setup

---
+++ src/main/java/com/musalasoft/droneapi/DroneApi.java
@@ -0,0 +1,11 @@
+package com.musalasoft.droneapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneApi {
+    public static void main(String[] args) {
+        SpringApplication.run(DroneApi.class, args);
+    }
+}
